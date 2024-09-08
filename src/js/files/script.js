e8cb79ce4a74ad8eae760c44bb4a54952184114d
Vadim Polovinko <vadimpolovinko89@gmail.com>
2024-09-08 15:12:18 +0300

    feat: add header for mobile

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,22 @@
 // Подключение функционала "Чертоги Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
+
+// Dropdown menu
+
+const dropdownLink = document.querySelectorAll('.menu__item-dropdown');
+
+if (dropdownLink) {
+  if (window.innerWidth < 991) {
+    dropdownLink.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const secondChild = link.children[1];
+        const firstChild = link.children[0];
+        secondChild.classList.toggle('sub-active');
+        firstChild.classList.toggle('link-active');
+      });
+    });
+  }
+}
