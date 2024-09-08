6a941ca6c2e6f76073af8263a61999e13cc66aca
Vadim Polovinko <vadimpolovinko89@gmail.com>
2024-09-09 00:05:12 +0300

    feat: add footer

--- src/js/app.js
+++ src/js/app.js
@@ -208,7 +208,7 @@ import * as flsScroll from './files/scroll/scroll.js';
 
 // Плавная навигация по странице
 // Документация: https://start-template.ru/modul-prokrutka-k-nuzhnomu-bloku-plavnaya-navigacziya-po-stranicze-chertogi-frilansera-v3-0-0/
-// flsScroll.pageNavigation();
+flsScroll.pageNavigation();
 
 // Функциональность добавления классов в хеддер при прокрутке
 // Документация: https://start-template.ru/modul-dobavleniya-klassov-v-shapku-pri-prokrutke-straniczy-chertogi-frilansera-v3-0-0/
